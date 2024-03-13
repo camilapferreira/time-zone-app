c4b0b30cbc764121d18ae1faf122a438e40314db
Camila Ferreira <pferreiracamila@gmail.com>
2024-03-13 22:32:10 +0100

    JS Update the timezones

--- src/script.js
+++ src/script.js
@@ -2,3 +2,12 @@
 // Tokyo 🇯🇵, Sydney 🇦🇺) and whenever a user selects one city, alert the current
 // date and time of the selected city such as : "It is Saturday, October 8,
 // 2023 2:51 PM in Europe/Paris"
+
+function displayTime(response) {
+  let timeZone = response.target.value;
+  let date = moment().tz(timeZone).format("dddd, MMMM D YYYY, h:mm A");
+  alert(`It is ${date} in ${timeZone}`);
+}
+
+let timezone = document.querySelector("#countries");
+timezone.addEventListener("change", displayTime);
