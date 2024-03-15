4e25448ffc387593a2bc2b1afce2400730058ae9
Camila Ferreira <pferreiracamila@gmail.com>
2024-03-15 15:53:09 +0100

    Udpdated the HTML

--- src/script.js
+++ src/script.js
@@ -1,13 +1,1 @@
-// Create an HTML file with a select listing 3 different cities (Paris 🇫🇷,
-// Tokyo 🇯🇵, Sydney 🇦🇺) and whenever a user selects one city, alert the current
-// date and time of the selected city such as : "It is Saturday, October 8,
-// 2023 2:51 PM in Europe/Paris"
-
-function displayTime(response) {
-  let timeZone = response.target.value;
-  let date = moment().tz(timeZone).format("dddd, MMMM D YYYY, h:mm A");
-  alert(`It is ${date} in ${timeZone}`);
-}
-
-let timezone = document.querySelector("#countries");
-timezone.addEventListener("change", displayTime);
+alert("Olá");
